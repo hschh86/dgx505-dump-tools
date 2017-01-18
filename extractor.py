e23e5bac3e9ff64a4ec7cbccc68d30e14a9081ce
xjh <hschh86@users.noreply.github.com>
2017-01-18 17:39:19 +1100

    initial conversions and tests

--- extractor.py
+++ extractor.py
@@ -1,5 +1,167 @@
 import mido
+import os.path
+
+
+# TEMPORARY -- DEPRECATE LATER
+def read_hex_syx(filename):
+    # workaround, to get avoid a bug? in mido 1.1.18
+    # (bytearray.fromhex doesn't like newlines...)
+    with open(filename, 'r', errors='replace') as infile:
+        hex_lines = infile.readlines()
+
+    # maybe... return mido.parse_all(itertools.chain(bytearray.fromhex(line) for line in data.splitlines())) ??
+    data = bytearray()
+    for line in hex_lines:
+        data.extend(bytearray.fromhex(line.strip()))
+    return [msg for msg in mido.Parser(data) if msg.type == 'sysex']
+
+
+# Utility for reading syx files.
+# Probably should be somewhere else
+def read_syx_file(infile):
+    # like mido.read_syx_file, but takes a binary mode file object instead.
+    with infile:
+        data = infile.read()
+
+    if data[0] != 0xf0:
+        # this makes a new copy of the entire data just to throw it away again... but i don't particularly care
+        data = bytearray.fromhex(data.translate(None, b'\t\n\r\f\v').decode(errors='replace'))
+    return mido.parse_all(data)
+
+# Fun Binary Tools
+def assert_low(byte):
+    """Raise ValueError if byte > 127"""
+    if byte > 0x7F:
+        raise ValueError("Byte value out of range: {}".format(byte))
+
+def unpack_seven(inbytes):
+    """Reconstruct a number from the seven-bit representation used in
+    the SysEx message data.
+    Takes a bytes-like object, where each byte is seven bits of the number
+    (big-endian byte order)
+    Each byte must have its high bit zero, or else ValueError is raised.
+    """
+    value = 0
+    for b in inbytes:
+        assert_low(b)
+        value = (value << 7) | b
+    return value
+
+# just for completeness' sake
+def seven_byte_length(value):
+    """Returns the minimum number of bytes required to represent the integer
+    if we can use seven bits per byte.
+    Positive integers only, please!"""
+    q, rem = divmod(value.bit_length(), 7)
+    if rem or not q: # (the not q is in case value is 0, we can't have 0 bytes)
+        q += 1
+    return q
+def pack_seven(value, length=None):
+    """Packs a positive integer value into the seven-bit representation used
+    in the sysex message data."""
+    if value < 0:
+        raise ValueError("Value is negative: {}".format(value))
+    minlen = seven_byte_length(value)
+    if length is None:
+        length = minlen
+    else:
+        # if 2**(7*length) < value...
+        if minlen > length:
+            raise ValueError("Length too short to fit value")
+    dest = bytearray(length)
+    for i in range(minlen):
+        dest[i] = (value & 0x7F)
+        value >>= 7
+    return bytes(reversed(dest))
 
 
 
-# basic 
+def unpack_variable_length(inbytes, limit=True):
+    """Reconstruct a number from the variable-length representation used
+    in Standard MIDI files. This version only accepts just the entire sequence
+    (that is, last byte must have high bit 0, all other bytes must have
+    high bit 1).
+    In actual MIDI files, the max length is four bytes. ValueError raised if
+    length of inbytes exceeds four. (set limit=False to override this)
+    """
+    if limit and len(inbytes) > 4:
+        raise ValueError("Sequence too long: {}".format(len(inbytes)))
+
+    value = 0
+    last = len(inbytes)-1
+    for i, b in enumerate(inbytes):
+        # check for validity
+        if (b > 0x7F) is not (i < last):
+            raise ValueError("Byte sequence not valid")
+        value = (value << 7) | (b & 0x7F)
+    return value
+
+def pack_variable_length(value, limit=True):
+    """Encode a positive integer as a variable-length number used in
+    Standard MIDI files.
+    ValueError rasied if value is over 0x0FFFFFFF (=would require >4 bytes).
+    Set limit=False to override this."""
+    if value < 0:
+        raise ValueError("Value is negative: {}".format(value))
+    if limit and value > 0x0FFFFFFF:
+        raise ValueError("Value too large: {}".format(value))
+
+    dest = bytearray()
+    dest.append(value & 0x7F)
+    value >>= 7
+    while value:
+        dest.append((value & 0x7F) | 0x80)
+        value >>= 7
+    return bytes(reversed(dest))
+
+def reconstitute(inbytes):
+    """Unpack a sequence of eight bytes into a bytearray of seven bytes
+    where the highest bit of each byte is determined by the eighth byte,
+    that is, unpack eight bytes of the bulk dump payload data"""
+    if len(inbytes) != 8:
+        raise ValueError("There must be eight bytes!")
+    dest = bytearray(7)
+    lastbyte = inbytes[7]
+    assert_low(lastbyte)
+    for i in range(7):
+        byte = inbytes[i]
+        assert_low(byte)
+        highbit = (lastbyte << (i+1)) & 0x80
+        dest[i] = byte | highbit
+    return dest
+
+def reconstitute_all(inbytes):
+    """Unpack a sequence with a length a multiple of eight using the
+    reconstitute function. Returns a bytes object."""
+    if len(inbytes) % 8 != 0:
+        raise ValueError("There must be a multiple of eight bytes!")
+    # would a memoryview object instead of a slice would be better here?
+    return b''.join(reconstitute(inbytes[i:i+8]) for i in range(0, len(inbytes), 8))
+
+
+def read_dgx_dump(messages):
+    """Takes a sequence of DGX-505 bulk dump sysex messages,
+    and tries to read em'.
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    import argparse
+    argparser = argparse.ArgumentParser(description="Extract UserSong MIDI files from a sysex dump")
+    ingroup = argparser.add_mutually_exclusive_group(required=True)
+    ingroup.add_argument('-p', '--port', type=str,
+                         help="Read from port (run 'mido-ports' to list available ports)")
+    ingroup.add_argument('-i', '--infile', type=str,
+                         help="Read from file")
+    argparser.add_argument('-o', '--outfile', type=str, required=True, help="Write to out file thing")
+    args = argparser.parse_args()
+
+    with open(args.infile, 'rb') as infile:
+        gs = read_syx_file(infile)
+    if os.path.exists(args.outfile):
+        # not 100% safe in the unlikely event of race conditions but it's good enough
+        raise FileExistsError("File exists: {!r}".format(args.outfile))
+    else:
+        mido.write_syx_file(args.outfile, gs) # if with file object support i should use x but whatever
---
+++ test_extractor.py
@@ -0,0 +1,67 @@
+import pytest
+
+import extractor as e
+
+def test_seven():
+    examples = [(b'\x35\x7E', (0x35 << 7) + 0x7E),
+                (b'\x10\x03\x00', 2**18 + (3<<7)),
+                (b'\x10\x00', 0x800),
+                (b'\x08\x68', 0x468),
+                (b'\x7F\x7F\x7F\x7F', 0x0FFFFFFF),
+                (b'\x00', 0)]
+    for a, b in examples:
+        assert e.unpack_seven(a) == b
+        assert e.pack_seven(b) == a
+        assert e.pack_seven(b, 5) == a.rjust(5, b'\x00')
+
+    assert e.unpack_seven(b'\x00'*5+b'\x3B') == 0x3B
+
+    with pytest.raises(ValueError):
+        e.unpack_seven(b'\x80')
+
+    with pytest.raises(ValueError):
+        e.pack_seven(-1)
+    with pytest.raises(ValueError):
+        e.pack_seven(2**8, length=1)
+
+def test_reconstitute():
+    examples = [(b'\x1A'*7+b'\x40', b'\x9A'+b'\x1A'*6),
+                (b'\x00'*7+b'\x7F', b'\x80'*7),
+                (b'\x00\x11\x23\x3F\x4F\x5C\x61\x4C',
+                 b'\x80\x11\x23\xBF\xCF\x5C\x61')]
+    for a, b in examples:
+        assert e.reconstitute(a) == b
+    for a in [b'\x00\x01', b'\x00\x00\x00\x90\x00\x00\x00\x00']:
+        with pytest.raises(ValueError):
+            e.reconstitute(a)
+
+    for seq in [(0, 1), (2, 1, 0), (2, 2, 0, 2)]:
+        a = b''.join(examples[n][0] for n in seq)
+        b = b''.join(examples[n][1] for n in seq)
+        assert e.reconstitute_all(a) == b
+    for a in [b'\xC0'*8, b'\x00'*12, b'\x00'*15+b'\xFF']:
+        with pytest.raises(ValueError):
+            e.reconstitute(a)
+
+def test_vl():
+    for a in [b'\x00', b'\x23', b'\x6E', b'\x7F']:
+        assert e.unpack_variable_length(a) == a[0]
+        assert e.pack_variable_length(a[0]) == a
+    examples = [(b'\xB5\x7E', (0x35 << 7) + 0x7E),
+                (b'\x90\x83\x00', 2**18 + (3<<7)),
+                (b'\x90\x00', 0x800),
+                (b'\x88\x68', 0x468),
+                (b'\xFF\xFF\xFF\x7F', 0x0FFFFFFF)]
+    for a, b in examples:
+        assert e.unpack_variable_length(a) == b
+        assert e.pack_variable_length(b) == a
+
+    a, b = (b'\x81\x80\x80\x80\x00', 2**28)
+    assert e.unpack_variable_length(a, False) == b
+    assert e.pack_variable_length(b, False) == a
+    with pytest.raises(ValueError):
+        assert e.unpack_variable_length(a)
+
+    for a in [-1, 2**28]:
+        with pytest.raises(ValueError):
+            e.pack_variable_length(a)
