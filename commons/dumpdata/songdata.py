d13c644ab7da68949f0413c9e72ba35a18468729
xjh <hschh86@users.noreply.github.com>
2017-12-24 23:18:56 +1100

    Add some comments

--- commons/dumpdata/songdata.py
+++ commons/dumpdata/songdata.py
@@ -26,7 +26,8 @@ class SongDumpSection(DumpSection):
 
 class SongData(CachedSequence):
     """
-    Container for all the useful data in a song section of a bulk dump
+    Container for all the useful data in a song section of a bulk dump.
+    Element access provides the usersongs (0-based indexing).
     """
     __slots__ = ('data', '_mystery', '_block_system')
 
@@ -97,15 +98,10 @@ class SongData(CachedSequence):
 
         super().__init__(5, make_song)
 
-    # def __getitem__(self, key):
-    #     """
-    #     Get the UserSong object.
-    #     Note that we use zero based indexing, so UserSong1 corresponds to [0]
-    #     and so on.
-    #     """
-    #     return self[key]
-
     def get_song(self, number):
+        """
+        Get song, 1-based indexing. (if you can call it that).
+        """
         if not (1 <= number <= 5):
             raise ValueError("song number out of range")
         return self[number-1]
@@ -116,6 +112,9 @@ class SongData(CachedSequence):
 
 
 class SongDataBlockSystem(object):
+    """
+    Helper object for getting tracks out of the 'blocksystem'.
+    """
 
     BLOCK_COUNT = 0x82
     BLOCK_SIZE = 0x200
@@ -144,7 +143,9 @@ class SongDataBlockSystem(object):
         return self._next_blocks_table[n-1]
 
     def _block_data_iter(self, start_block, length):
-        """Yields data blocks up to length from start_block"""
+        """
+        Yields data blocks up to length from start_block
+        """
         num = start_block
         rem = length
         while rem > 0:
@@ -155,6 +156,7 @@ class SongDataBlockSystem(object):
             block = self.get_block_data(num)
             if rem < self.BLOCK_SIZE:
                 block = block[:rem]
+                # We don't want to read too much, so chop off the end
             rem -= len(block)
             num = self.get_next_block_number(num)
             yield block
@@ -168,13 +170,19 @@ class SongDataBlockSystem(object):
         blocks is a list of the blocks (as memoryviews, with the last one
         truncated appropriately for the chunk size)
         """
+        # First, we need to check if this is actually a block.
         try:
             block = self.get_block_data(start_block)
         except IndexError:
             raise MalformedDataError("Invalid starting block")
+        # Then, we see if the block indeed contains the start of a track:
+        # b'MTrk' followed by 32 bit integer, the length remaining
         tag, dlength = struct.unpack_from('>4sL', block, 0)
         if tag != b'MTrk':
             raise MalformedDataError("Chunk start not found")
+        # The start of the track chunk tells us its remaining length
+        # i.e. not including the 8-byte header.
+        # so, we need to add 8 to get the total size of the chunk
         size = dlength + 8
         blocks = list(self._block_data_iter(start_block, size))
         return size, blocks
@@ -196,9 +204,13 @@ class UserSong(object):
 
         self.name = "User Song {}".format(number)
 
+        # create the tracks.
         self._tracks = []
         # transpose the last track to first so that
         # index 0 = time/chord track A, index 1 = track 1 etc
+        # transposing the tracks like that makes the indices match up
+        # and also puts the time track (A) first, which is helpful for
+        # reconstructing MIDI files.
         TRACK_NAMES = ('Track 1', 'Track 2', 'Track 3',
                        'Track 4', 'Track 5', 'Track A')
         for i in range(-1, 5):
@@ -213,8 +225,11 @@ class UserSong(object):
                                        size, blocks)
             self._tracks.append(track)
 
+        # create a list of the tracks that are actually recorded
         self._datatracks = [track for track in self._tracks
                             if track.blocks is not None]
+        # Calculate the size of the resulting smf file
+        # i.e. total track sizes plus 14 byte MThd chunk
         if self._datatracks:
             self.size = 14 + sum(track.size for track in self._datatracks)
         else:
@@ -234,17 +249,24 @@ class UserSong(object):
             print(columns(item.name, item.active, item.duration, item.size))
 
     def _midi_blocks_iter(self):
+        # yield the blocks for a Type 1 smf MIDI file.
         if not self._datatracks:
             raise NotRecordedError("Song not recorded")
+        # Construct the header:
+        # chunk length 6, MIDI file format 1, 96 ticks per quarter note.
         header = struct.pack('>4sL3H',
                              b'MThd', 6, 1, len(self._datatracks), 96)
+        # yield header then all the blocks for all the tracks.
+        # This is why Track A went first.
         yield header
         for track in self._datatracks:
             yield from track.blocks
 
     @lazy_property
     def midi(self):
-        """The MIDI file, as bytes."""
+        """
+        The MIDI file, as bytes.
+        """
         return b''.join(self._midi_blocks_iter())
 
     def _cereal(self):
