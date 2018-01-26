e7c5ff934e6b753add936afc80f5daaade0f0fd8
xjh <hschh86@users.noreply.github.com>
2018-01-27 02:05:38 +1100

    Add an option for polling instead of callback to slurp_rtmidi

--- slurp_rtmidi.py
+++ slurp_rtmidi.py
@@ -10,6 +10,7 @@ of the rtmidi's timestamp slightly more directly.
 import sys
 import argparse
 import logging
+import time
 
 import mido
 import rtmidi
@@ -30,10 +31,11 @@ portargs.add_argument(
 portargs.add_argument(
     '-V', '--virtual', action='store_true',
     help='Use virtual port')
-#
-# argparser.add_argument(
-#     '-c', '--clocktime', action='store_true',
-#     help="Use the clock (since epoch) time instead of elapsed time")
+
+argparser.add_argument(
+    '-P', '--poll', type=float, nargs='?', default=None, const=0.25,
+    help="Use polling instead of callback. "
+         "Optionally specify interval, in seconds, default is 0.25")
 
 argparser.add_argument(
     '-q', '--quiet', action='store_true',
@@ -41,9 +43,12 @@ argparser.add_argument(
 
 
 def new_callback():
+    # Note the difference between this callback and the one in slurp.py,
+    # which counts the time elapsed since the callback was created;
+    # this one counts the time since the first message received.
     acc = 0
 
-    def rtmidi_callback(msg_delta, data):
+    def rtmidi_callback(msg_delta, data=None):
         nonlocal acc
         # Use this as the callback, which runs in the rtmidi input thread.
         # (I think.)
@@ -70,7 +75,10 @@ def new_callback():
 
 
 def open_input(rt, name, guess, virtual):
-
+    """
+    Open an input port on the instance rt.
+    Returns the name of the port.
+    """
     if virtual:
         # Open a virtual port.
         rt.open_virtual_port(name)
@@ -101,6 +109,19 @@ def open_input(rt, name, guess, virtual):
         return port_name
 
 
+def flush_messages(rt, callback):
+    # deal with the callback manually.
+    # probably neater to rework everything into a class instead of
+    # throwing rt and callback around all the time, but hey, it works.
+    while True:
+        msg_delta = rt.get_message()
+        if msg_delta is None:
+            # no messages, return
+            return
+        else:
+            callback(msg_delta)
+
+
 def main(args):
     logger = logging.getLogger('slurp_rtmidi')
 
@@ -113,32 +134,50 @@ def main(args):
     # we only want to ignore active sense.
     rt.ignore_types(sysex=False, timing=False, active_sense=True)
 
-    # create and set the callback.
-    rt.set_callback(new_callback())
-    # Note the difference between this callback and the one in slurp.py,
-    # which counts the time elapsed since the callback was created;
-    # this one counts the time since the first message received.
+    # create the callback
+    callback = new_callback()
+    # Callback or poll?
+    if args.poll is None:
+        # set the callback.
+        rt.set_callback(callback)
+    else:
+        # we're going to be polling the interval,
+        # calling the callback ourselves.
+        if args.poll <= 0:
+            raise ValueError("Polling interval must be positive")
 
     # Now we need to open the port.
     port_name = open_input(rt, args.port, args.guessport, args.virtual)
 
     try:
-        logger.info('Reading from port %r. Press enter to stop',
+        logger.info('Reading from port %r. CtrlC to stop',
                     port_name)
-        # wait for any user input
-        input()
+        if args.poll is None:
+            # we've set the callback, so we just sleep
+            while True:
+                time.sleep(300)
+        else:
+            # we need to poll.
+            while True:
+                flush_messages(rt, callback)
+                time.sleep(args.poll)
     except KeyboardInterrupt:
         # ctrl-c
         logger.info('Stopping on KeyboardInterrupt')
-    except EOFError:
-        # ctrl-d or eof if a file is piped in for some reason
-        logger.info('Stopping on input EOF')
-    else:
-        # input on stdin
-        logger.info('Stopping on input')
     finally:
-        # just in case
+        # close the port. (Also cancels the callback)
+        rt.close_port()
+        # Deal with the remaining messages
+        flush_messages(rt, callback)
+        # note: virtual ports don't actually close until rt is deleted
+        # so there is the possibility that messages keep coming in.
+        # It is then possible that we can't handle them fast enough,
+        # so flush_messages keeps running forever.
+        # in that case, we'd need to interrupt again.
         del rt
+        # It seems that when the message buffer fills up, an error
+        # message is just printed to stderr,
+        # with no (reasonable) way to catch it.
 
 
 if __name__ == '__main__':
