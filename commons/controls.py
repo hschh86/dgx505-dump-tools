c7f37308a41e114152deb14a06b1d141643098f3
xjh <hschh86@users.noreply.github.com>
2019-01-02 00:03:13 +1100

    roll controls.csv info back into controls.py

--- commons/controls.py
+++ commons/controls.py
@@ -6,6 +6,42 @@ For working with the controls / sysex / etc midi messages
 
 import mido
 
+controls = {}
+control_names = {}
+for short, num, name in (
+    ("bank_msb",           0x00, "Bank MSB"),
+    ("bank_lsb",           0x32, "Bank LSB"),
+    ("volume",             0x07, "Voice Volume"),
+    ("pan",                0x0A, "Voice Pan"),
+    ("reverb",             0x5B, "Voice Reverb Level"),
+    ("chorus",             0x5D, "Voice Chorus Level"),
+    ("pedal",              0x40, "Pedal Sustain"),
+    ("release",            0x48, "Release Time"),
+    ("modulation",         0x01, "Modulation Wheel"),
+    ("expression",         0x0B, "Expression"),
+    ("portamento_ctrl",    0x54, "Portamento Control"),
+    ("harmonic",           0x47, "Harmonic Content"),
+    ("attack",             0x49, "Attack Time"),
+    ("brightness",         0x4A, "Brightness"),
+    ("rpn_msb",            0x65, "RPN MSB"),
+    ("rpn_lsb",            0x64, "RPN LSB"),
+    ("data_msb",           0x06, "Data Entry MSB"),
+    ("data_lsb",           0x26, "Data Entry LSB"),
+    ("data_inc",           0x60, "Data Increment"),
+    ("data_dec",           0x61, "Data Decrement"),
+    ("sound_off",          0x78, "All Sound OFF"),
+    ("sound_off_xmono",    0x7E, "All Sound OFF (MONO)"),
+    ("sound_off_xpoly",    0x7F, "All Sound OFF (POLY)"),
+    ("notes_off",          0x7B, "All Notes OFF"),
+    ("notes_off_xomnioff", 0x7C, "All Notes OFF (OMNI OFF)"),
+    ("notes_off_xomnion",  0x7D, "All Notes OFF (OMNI ON)"),
+    ("reset_controls",     0x79, "Reset All Controls"),
+    ("local",              0x7A, "Local ON/OFF"),
+):
+    controls[short] = num
+    control_names[num] = name
+
+
 
 def reverb(msb, lsb):
     return mido.Message(
@@ -71,34 +107,3 @@ def multisend(port, messages):
     for message in messages:
         port.send(message)
 
-
-controls = {
-    'bank_msb': 0x00,
-    'bank_lsb': 0x32,
-    'volume': 0x07,
-    'pan': 0x0A,
-    'reverb': 0x5B,
-    'chorus': 0x5D,
-    'pedal': 0x40,
-    'release': 0x48,
-    'modulation': 0x01,
-    'expression': 0x0B,
-    'portamento_ctrl': 0x54,
-    'harmonic': 0x47,
-    'attack': 0x49,
-    'brightness': 0x4A,
-    'rpn_msb': 0x65,
-    'rpn_lsb': 0x64,
-    'data_msb': 0x06,
-    'data_lsb': 0x26,
-    'data_inc': 0x60,
-    'data_dec': 0x61,
-    'sound_off': 0x78,
-    'sound_off_xmono': 0x7E,
-    'sound_off_xpoly': 0x7F,
-    'notes_off': 0x7B,
-    'notes_off_xomnioff': 0x7C,
-    'notes_off_xomnion': 0x7D,
-    'reset_controls': 0x79,
-    'local': 0x7A
-}
