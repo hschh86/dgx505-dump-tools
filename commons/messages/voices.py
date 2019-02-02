2c5dceee5b01a9649264ce2e9bbd74656a96796d
xjh <hschh86@users.noreply.github.com>
2019-02-02 23:21:08 +1100

    implement voice fallback

--- commons/messages/voices.py
+++ commons/messages/voices.py
@@ -17,13 +17,16 @@ class Voice(collections.namedtuple("Voice",
         if self.number is None:
             n = "???"
         else:
-            n = format(self.number, "03d")        
+            n = format(self.number, "03d")
         return "{} {} ({})".format(n, self.name, self.category)
 
     def voice_string_extended(self):
         return "[{},{},{}] {}".format(
             self.msb, self.lsb, self.prog, self.voice_string())
 
+# The Silent None Voice.
+# must redo this properly.....
+SILENT = Voice(None, None, None, None, None, None)
 
 # Useless Class Strikes Again
 
@@ -31,7 +34,7 @@ class _VoiceLookup(object):
 
     DictTuple = collections.namedtuple("DictTuple",
         "numbers bank_programs names_nonxg names_xg")
-    
+
     @util.lazy_property
     def dicts(self):
         _numbers = {}
@@ -58,13 +61,13 @@ class _VoiceLookup(object):
         assert (len(_numbers)
                 == len(_bank_programs)
                 == len(_names_nonxg) + len(_names_xg))
-        
+
         return self.DictTuple(_numbers, _bank_programs, _names_nonxg, _names_xg)
 
     @util.lazy_property
     def names_nonxg_first(self):
         return collections.ChainMap(self.dicts.names_nonxg, self.dicts.names_xg)
-    
+
     @util.lazy_property
     def names_xg_first(self):
         return collections.ChainMap(self.dicts.names_xg, self.dicts.names_nonxg)
@@ -108,11 +111,33 @@ def from_bank_program(msb, lsb, prog):
 def from_bank_program_default(msb, lsb, prog):
     """
     Look up a voice by bank and program bytes, using from_bank_program
-    if no voice has the bytes, then it will try to fall back to generic
+    if no voice has the bytes, then it will try to fall back to the voice.
+    Returns None to indicate no change to the voice,
+    returns SILENT to indicate that the voice is deactivated,
+    returns a regular voice.
     """
-    # This needs to be redone properly once I figure out how
-    try:
-        return from_bank_program(msb, lsb, prog)
-    except KeyError:
-        # Just use a basic default for now.
-        return Voice(None, None, None, msb, lsb, prog)
+    # The behaviour of a program change depends on the bank MSB.
+    if msb == 0x7F:  # Drum Kit. Ignore LSB.
+        try:
+            return from_bank_program(msb, 0, prog)
+        except KeyError:
+            # If no match, then don't change at all.
+            # we'll return None to indicate this.
+            return None
+    elif msb == 0x7E: # SFX Kit. Exact Match?
+        try:
+            return from_bank_program(msb, lsb, prog)
+        except KeyError:
+            # We do have a voice, but it's the null silent voice.
+            return SILENT
+    else: # Voices
+        try:
+            # First, try all
+            return from_bank_program(msb, lsb, prog)
+        except KeyError:
+            try:
+                # Then, fallback to LSB 0
+                return from_bank_program(msb, 0, prog)
+            except KeyError:
+                # Finally use SILENT.
+                return SILENT
