fe18fe82ba4d3aa380fe13b0813fbf19ac8d6b89
xjh <hschh86@users.noreply.github.com>
2019-02-03 02:33:54 +1100

    Move the enums in control over to wrappers

--- commons/messages/controls.py
+++ commons/messages/controls.py
@@ -4,125 +4,12 @@ controls.py
 For working with the controls / sysex / etc midi messages
 """
 
-import enum
 import mido
 
 from . import voices
 
 from ..util import lazy_property
-
-
-class MessageType(enum.Enum):
-    NOTE_ON = "note_on"
-    NOTE_OFF = "note_off"
-    PITCHWHEEL = "pitchwheel"
-    PROGRAM_CHANGE = "program_change"
-    CONTROL_CHANGE = "control_change"
-    SYSEX = "sysex"
-    CLOCK = "clock"
-    START = "start"
-    STOP = "stop"
-
-
-class Control(enum.Enum):
-    BANK_MSB = 0x00
-    BANK_LSB = 0x20
-    VOLUME = 0x07
-    PAN = 0x0A
-    REVERB = 0x5B
-    CHORUS = 0x5D
-    PEDAL = 0x40
-    RELEASE = 0x48
-    MODULATION = 0x01
-    EXPRESSION = 0x0B
-    PORTAMENTO_CTRL = 0x54
-    HARMONIC = 0x47
-    ATTACK = 0x49
-    BRIGHTNESS = 0x4A
-    RPN_MSB = 0x65
-    RPN_LSB = 0x64
-    DATA_MSB = 0x06
-    DATA_LSB = 0x26
-    DATA_INC = 0x60
-    DATA_DEC = 0x61
-    SOUND_OFF = 0x78
-    SOUND_OFF_XMONO = 0x7E
-    SOUND_OFF_XPOLY = 0x7F
-    NOTES_OFF = 0x7B
-    NOTES_OFF_XOMNIOFF = 0x7C
-    NOTES_OFF_XOMNION = 0x7D
-    RESET_CONTROLS = 0x79
-    LOCAL = 0x7A
-
-
-class Rpn(enum.Enum):
-    PITCH_BEND_RANGE = (0x00, 0x00)
-    FINE_TUNE = (0x00, 0x01)
-    COARSE_TUNE = (0x00, 0x02)
-    NULL = (0x7F, 0x7F)
-
-
-class SysEx(enum.Enum):
-    GM_ON = "GM System ON"
-    MASTER_VOL = "MIDI Master Volume"
-    MASTER_TUNE = "MIDI Master Tuning"
-    REVERB_TYPE = "Reverb Type"
-    CHORUS_TYPE = "Chorus Type"
-    XG_ON = "XG System ON"
-    XG_RESET = "XG All Parameter Reset"
-
-
-longform = {
-    MessageType.NOTE_ON:        "Note On",
-    MessageType.NOTE_OFF:       "Note Off",
-    MessageType.PITCHWHEEL:     "Pitch Bend",
-    MessageType.PROGRAM_CHANGE: "Program Change",
-    MessageType.CONTROL_CHANGE: "Control Change",
-    MessageType.SYSEX:          "System Exclusive",
-    MessageType.CLOCK:          "Clock",
-    MessageType.START:          "Start",
-    MessageType.STOP:           "Stop",
-    Control.BANK_MSB:           "Bank MSB",
-    Control.BANK_LSB:           "Bank LSB",
-    Control.VOLUME:             "Voice Volume",
-    Control.PAN:                "Voice Pan",
-    Control.REVERB:             "Voice Reverb Level",
-    Control.CHORUS:             "Voice Chorus Level",
-    Control.PEDAL:              "Pedal Sustain",
-    Control.RELEASE:            "Release Time",
-    Control.MODULATION:         "Modulation Wheel",
-    Control.EXPRESSION:         "Expression",
-    Control.PORTAMENTO_CTRL:    "Portamento Control",
-    Control.HARMONIC:           "Harmonic Content",
-    Control.ATTACK:             "Attack Time",
-    Control.BRIGHTNESS:         "Brightness",
-    Control.RPN_MSB:            "RPN MSB",
-    Control.RPN_LSB:            "RPN LSB",
-    Control.DATA_MSB:           "Data Entry MSB",
-    Control.DATA_LSB:           "Data Entry LSB",
-    Control.DATA_INC:           "Data Increment",
-    Control.DATA_DEC:           "Data Decrement",
-    Control.SOUND_OFF:          "All Sound OFF",
-    Control.SOUND_OFF_XMONO:    "All Sound OFF (MONO)",
-    Control.SOUND_OFF_XPOLY:    "All Sound OFF (POLY)",
-    Control.NOTES_OFF:          "All Notes OFF",
-    Control.NOTES_OFF_XOMNIOFF: "All Notes OFF (OMNI OFF)",
-    Control.NOTES_OFF_XOMNION:  "All Notes OFF (OMNI ON)",
-    Control.RESET_CONTROLS:     "Reset All Controllers",
-    Control.LOCAL:              "Local ON/OFF",
-    Rpn.PITCH_BEND_RANGE:       "Pitch Bend Range",
-    Rpn.FINE_TUNE:              "Channel Fine Tuning",
-    Rpn.COARSE_TUNE:            "Channel Coarse Tuning",
-    Rpn.NULL:                   "Null",
-    SysEx.GM_ON:                "GM System ON",
-    SysEx.MASTER_VOL:           "MIDI Master Volume",
-    SysEx.MASTER_TUNE:          "MIDI Master Tuning",
-    SysEx.REVERB_TYPE:          "Reverb Type",
-    SysEx.CHORUS_TYPE:          "Chorus Type",
-    SysEx.XG_ON:                "XG System ON",
-    SysEx.XG_RESET:             "XG All Parameter Reset",
-}
-
+from .wrappers import Control, Rpn
 
 def xg_parameter_change(*args, n=0):
     if n >> 4 != 0:
--- commons/messages/wrappers.py
+++ commons/messages/wrappers.py
@@ -6,328 +6,171 @@ Wrappers for mido messages.
 The intent of these is to have a more friendly way of
 displaying and working with control and sysex messages.
 """
-# I'm not sure if that goal is achieved. May have gone overboard with classes
 
-# Should these be wrappers, subclasses, or entirely different???
-import re
+import enum
+from collections import namedtuple
 
-from .controls import MessageType, Control, Rpn, SysEx, longform
-from ..enums import ReverbType, ChorusType, SwitchBool, NoteValue
-from . import voices
+class _LongformEnum(enum.Enum):
+    def __str__(self):
+        return _LONGFORM_MAP.get(self, super().__str__())
+
+
+class MessageType(_LongformEnum):
+    NOTE_ON = "note_on"
+    NOTE_OFF = "note_off"
+    PITCHWHEEL = "pitchwheel"
+    PROGRAM_CHANGE = "program_change"
+    CONTROL_CHANGE = "control_change"
+    SYSEX = "sysex"
+    CLOCK = "clock"
+    START = "start"
+    STOP = "stop"
+
+
+class Control(_LongformEnum):
+    BANK_MSB = 0x00
+    BANK_LSB = 0x20
+    VOLUME = 0x07
+    PAN = 0x0A
+    REVERB = 0x5B
+    CHORUS = 0x5D
+    PEDAL = 0x40
+    RELEASE = 0x48
+    MODULATION = 0x01
+    EXPRESSION = 0x0B
+    PORTAMENTO_CTRL = 0x54
+    HARMONIC = 0x47
+    ATTACK = 0x49
+    BRIGHTNESS = 0x4A
+    RPN_MSB = 0x65
+    RPN_LSB = 0x64
+    DATA_MSB = 0x06
+    DATA_LSB = 0x26
+    DATA_INC = 0x60
+    DATA_DEC = 0x61
+    SOUND_OFF = 0x78
+    SOUND_OFF_XMONO = 0x7E
+    SOUND_OFF_XPOLY = 0x7F
+    NOTES_OFF = 0x7B
+    NOTES_OFF_XOMNIOFF = 0x7C
+    NOTES_OFF_XOMNION = 0x7D
+    RESET_CONTROLS = 0x79
+    LOCAL = 0x7A
+
+
+class Rpn(_LongformEnum):
+    PITCH_BEND_RANGE = (0x00, 0x00)
+    FINE_TUNE = (0x00, 0x01)
+    COARSE_TUNE = (0x00, 0x02)
+    NULL = (0x7F, 0x7F)
+
+
+class SysEx(_LongformEnum):
+    GM_ON = "GM System ON"
+    MASTER_VOL = "MIDI Master Volume"
+    MASTER_TUNE = "MIDI Master Tuning"
+    REVERB_TYPE = "Reverb Type"
+    CHORUS_TYPE = "Chorus Type"
+    XG_ON = "XG System ON"
+    XG_RESET = "XG All Parameter Reset"
+
+
+_LONGFORM_MAP = {
+    MessageType.NOTE_ON:        "Note On",
+    MessageType.NOTE_OFF:       "Note Off",
+    MessageType.PITCHWHEEL:     "Pitch Bend",
+    MessageType.PROGRAM_CHANGE: "Program Change",
+    MessageType.CONTROL_CHANGE: "Control Change",
+    MessageType.SYSEX:          "System Exclusive",
+    MessageType.CLOCK:          "Clock",
+    MessageType.START:          "Start",
+    MessageType.STOP:           "Stop",
+    Control.BANK_MSB:           "Bank MSB",
+    Control.BANK_LSB:           "Bank LSB",
+    Control.VOLUME:             "Voice Volume",
+    Control.PAN:                "Voice Pan",
+    Control.REVERB:             "Voice Reverb Level",
+    Control.CHORUS:             "Voice Chorus Level",
+    Control.PEDAL:              "Pedal Sustain",
+    Control.RELEASE:            "Release Time",
+    Control.MODULATION:         "Modulation Wheel",
+    Control.EXPRESSION:         "Expression",
+    Control.PORTAMENTO_CTRL:    "Portamento Control",
+    Control.HARMONIC:           "Harmonic Content",
+    Control.ATTACK:             "Attack Time",
+    Control.BRIGHTNESS:         "Brightness",
+    Control.RPN_MSB:            "RPN MSB",
+    Control.RPN_LSB:            "RPN LSB",
+    Control.DATA_MSB:           "Data Entry MSB",
+    Control.DATA_LSB:           "Data Entry LSB",
+    Control.DATA_INC:           "Data Increment",
+    Control.DATA_DEC:           "Data Decrement",
+    Control.SOUND_OFF:          "All Sound OFF",
+    Control.SOUND_OFF_XMONO:    "All Sound OFF (MONO)",
+    Control.SOUND_OFF_XPOLY:    "All Sound OFF (POLY)",
+    Control.NOTES_OFF:          "All Notes OFF",
+    Control.NOTES_OFF_XOMNIOFF: "All Notes OFF (OMNI OFF)",
+    Control.NOTES_OFF_XOMNION:  "All Notes OFF (OMNI ON)",
+    Control.RESET_CONTROLS:     "Reset All Controllers",
+    Control.LOCAL:              "Local ON/OFF",
+    Rpn.PITCH_BEND_RANGE:       "Pitch Bend Range",
+    Rpn.FINE_TUNE:              "Channel Fine Tuning",
+    Rpn.COARSE_TUNE:            "Channel Coarse Tuning",
+    Rpn.NULL:                   "Null",
+    SysEx.GM_ON:                "GM System ON",
+    SysEx.MASTER_VOL:           "MIDI Master Volume",
+    SysEx.MASTER_TUNE:          "MIDI Master Tuning",
+    SysEx.REVERB_TYPE:          "Reverb Type",
+    SysEx.CHORUS_TYPE:          "Chorus Type",
+    SysEx.XG_ON:                "XG System ON",
+    SysEx.XG_RESET:             "XG All Parameter Reset",
+}
+
+
+class UnknownControl(namedtuple('UnknownControl', 'value')):   
+    def __str__(self):
+        return "[Control {}]".format(self.value)
+    
+
+class UnknownSysEx(namedtuple('UnknownSysEx', 'value')):
+    def __str__(self):
+        return "[SysEx {}]".format(" ".join(
+            format(b, "02X") for b in self.value
+        ))
+
+class UnknownRpn(namedtuple('UnknownRpn', 'value')):
+    def __str__(self):
+        return "[RPN {:02X} {:02X}".format(*self.value)
+
+class RpnDataCombo(namedtuple('RpnDataCombo', 'control rpn')):
+    def __str__(self):
+        return "{0.control!s}: {0.rpn!s}".format(self)
+
 
 class WrappedMessage(object):
-    # These are wrapped mido messages.
-    # Like normal messages, but with some extra metadata.
-    wrap_type = None
-    value = None
 
-    def __init__(self, message):
+    def __init__(self, wrap_type=None, value=None, 
+                 message=None, no_channel=False):
         """
         Wrap a mido message.
         """
         # I suppose we could use FrozenMessages here,
         # but we are all responsible adults, right?
+        self.wrap_type = wrap_type
+        self.value = value
         self.message = message
-
-    @property
-    def channel(self):
-        try:
-            return self.message.channel
-        except AttributeError:
-            return None
-
-    @property
-    def wrap_type_longform(self):
-        if self.wrap_type is None:
-            try:
-                return longform.get(
-                    MessageType(self.message.type))
-            except ValueError:
-                return self.message.type
-        return longform.get(self.wrap_type)
-
-    @property
-    def value_longform(self):
-        return self.value
+        if no_channel:
+            self.channel = None
+        else:
+            self.channel = self.message.channel
 
     def __str__(self):
         return " ".join(
             str(i) for i in (
-                self.channel, self.wrap_type_longform, self.value_longform)
+                self.channel, self.wrap_type, self.value)
             if i is not None)
 
     def __repr__(self):
         return "<{!s} {!r}>".format(self, self.message)
 
 
-class WrappedGlobalMessage(WrappedMessage):
-    # A wrapped message that changes the global state.
-    @property
-    def channel(self):
-        return None
-
-
-class WrappedSysEx(WrappedGlobalMessage):
-    wrap_type = MessageType.SYSEX
-
-    def __init__(self, message, match=None):
-        super().__init__(message)
-        self._process(match)
-
-    def _process(self, match):
-        self.value = None
-
-    @property
-    def wrap_type_longform(self):
-        if self.wrap_type is MessageType.SYSEX:
-            return "[SysEx {}]".format(
-                " ".join(format(x, "02X") for x in self.message.data))
-        else:
-            return super().wrap_type_longform
-
-
-class WrappedGMSystemOn(WrappedSysEx):
-    # GM System ON, F0 7E 7F 09 01 F7
-    wrap_type = SysEx.GM_ON
-    REGEX = re.compile(rb'\x7E\x7F\x09\x01', re.S)
-
-
-class WrappedMIDIMasterVolume(WrappedSysEx):
-    # MIDI Master Volume, F0 7F 7F 04 01 ** mm F7
-    wrap_type = SysEx.MASTER_VOL
-    REGEX = re.compile(rb'\x7F\x7F\x04\x01.(.)', re.S)
-    def _process(self, match):
-        self.value, = match.group(1)
-
-
-class WrappedMIDIMasterTuning(WrappedSysEx):
-    # MIDI Master Tuning, F0 43 1* 27 30 00 00 *m *l ** F7
-    wrap_type = SysEx.MASTER_TUNE
-    REGEX = re.compile(rb'\x43[\x10-\x1F]\x27\x30\x00\x00(..).', re.S)
-    def _process(self, match):
-        self.msb, self.lsb = match.group(1)
-        # we take the least significant nybble of each.
-        ml = ((self.msb & 0xF) << 4) | (self.lsb & 0xF)
-        # subtract 0x80 and clamp to range -100 +100
-        self.value = max(-100, min(ml-0x80, +100))
-
-
-class WrappedReverbChorus(WrappedSysEx):
-    # Reverb Type, F0 43 1n 4C 02 01 00 mm ll F7
-    # Chorus Type, F0 43 1n 4C 02 01 20 mm ll F7
-    REGEX = re.compile(rb'\x43[\x10-\x1F]\x4C\x02\x01([\x00\x20]..)', re.S)
-    def _process(self, match):
-        cat, self.msb, self.lsb = match.group(1)
-        if cat == 0x00:
-            self.wrap_type = SysEx.REVERB_TYPE
-            self.value = ReverbType.from_b(self.msb, self.lsb)
-        elif cat == 0x20:
-            self.wrap_type = SysEx.CHORUS_TYPE
-            self.value = ChorusType.from_b(self.msb, self.lsb)
-
-
-def wrap_sysex(message):
-
-    # Put the data into a bytes object so we can regex it
-    data = bytes(message.data)
-    # Match one by one
-    for sysex_class in (WrappedGMSystemOn, WrappedMIDIMasterVolume,
-            WrappedMIDIMasterTuning, WrappedReverbChorus):
-        match = sysex_class.REGEX.fullmatch(data)
-        if match is not None:
-            return sysex_class(message, match)
-    # if not, just wrap it generically
-    return WrappedSysEx(message)
-
-
-class WrappedControlChange(WrappedMessage):
-    wrap_type = MessageType.CONTROL_CHANGE
-    def __init__(self, message):
-        super().__init__(message)
-
-        try:
-            self.wrap_type = Control(self.message.control)
-        except ValueError:
-            pass
-
-        self._process()
-
-    @property
-    def wrap_type_longform(self):
-        if self.wrap_type is MessageType.CONTROL_CHANGE:
-            return "[Control {}]".format(self.message.control)
-        else:
-            return super().wrap_type_longform
-
-
-    def _process(self):
-        self.value = self.message.value
-
-
-
-class WrappedSingle(WrappedControlChange):
-    TYPES = {
-        Control.DATA_INC, Control.DATA_DEC,
-        Control.SOUND_OFF, Control.SOUND_OFF_XMONO, Control.SOUND_OFF_XPOLY,
-        Control.NOTES_OFF, Control.NOTES_OFF_XOMNIOFF, Control.NOTES_OFF_XOMNION
-    }
-    def _process(self):
-        # No use for the value
-        self.value = None
-
-    def __str__(self):
-        return "{} {!s}".format(self.channel, self.wrap_type_longform)
-
-
-class WrappedHighBoolean(WrappedControlChange):
-    def _process(self):
-        # highest bit: 1 for ON, 0 for OFF.
-        self.value = SwitchBool(self.message.value >= 64)
-
-class WrappedPedal(WrappedHighBoolean):
-    TYPES = {Control.PEDAL}
-
-
-class WrappedLocal(WrappedGlobalMessage, WrappedHighBoolean):
-    TYPES = {Control.LOCAL}
-    def __str__(self):
-        return "{!s} {}".format(self.wrap_type_longform, self.value)
-
-
-_CONTROL_WRAP_MAPPING = {t: c
-    for c in (WrappedSingle, WrappedPedal, WrappedLocal)
-    for t in c.TYPES}
-
-
-def wrap_control(message):
-    control_class = _CONTROL_WRAP_MAPPING.get(
-        message.control, WrappedControlChange)
-    return control_class(message)
-
-
-class WrappedProgramChange(WrappedMessage):
-    wrap_type = MessageType.PROGRAM_CHANGE
-
-    def __init__(self, message):
-        super().__init__(message)
-        self.value = message.program
-
-
-class WrappedPitchwheel(WrappedMessage):
-    wrap_type = MessageType.PITCHWHEEL
-
-    def __init__(self, message):
-        super().__init__(message)
-        self.value = message.pitch
-
-
-class WrappedNote(WrappedMessage):
-    wrap_type = MessageType.NOTE_ON
-
-    def __init__(self, message):
-        super().__init__(message)
-        self.value = NoteValue(message.note)
-        # Not the cleanest way to do it.
-        # Should we fold velocity 0 into Note_OFF?
-        if message.type == "note_off" or message.velocity == 0:
-            self.wrap_type = MessageType.NOTE_OFF
-    
-
-_MESSAGE_WRAP_MAPPING = {
-    "program_change": WrappedProgramChange,
-    "control_change": wrap_control,
-    "sysex": wrap_sysex,
-    "pitchwheel": WrappedPitchwheel,
-    "note_on": WrappedNote,
-    "note_off": WrappedNote
-}
-
-def wrap(message):
-    """
-    Wrap a message.
-    If a message is not of the wrappable type,
-    then returns None.
-    """
-    wrapper = _MESSAGE_WRAP_MAPPING.get(message.type, WrappedMessage)
-    return wrapper(message)
-
-
-class StateChange(WrappedMessage):
-    """
-    This class is for special wrappers in the control-state thing.
-    The wrappers wrap a wrapper itself. This wrapper is the wrapper
-    that triggered the shape change.
-    """
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-
-    @property
-    def message(self):
-        return self.wrapped.message
-
-    @property
-    def channel(self):
-        return self.wrapped.channel
-
-
-class DataChange(StateChange):
-    """
-    StateChange object for when some rpn data is set.
-    """
-
-    def __init__(self, wrapped, rpn, data):
-        super().__init__(wrapped)
-        self.rpn = rpn
-        self.data = data
-
-        try:
-            wrap_rpn = Rpn(rpn)
-        except ValueError:
-            wrap_rpn = None
-        self.wrap_type = (wrapped.wrap_type, wrap_rpn)
-
-        self._process()
-
-    def _wrap_rpn_longform(self):
-        wrap_rpn = self.wrap_type[1]
-        if wrap_rpn is not None:
-            try:
-                return longform[wrap_rpn]
-            except KeyError:
-                pass
-        return "[RPN {}]".format(self.rpn)
-
-    @property
-    def wrap_type_longform(self):
-        return "{}: {}".format(
-            self.wrapped.wrap_type_longform, self._wrap_rpn_longform())
-
-
-    def _process(self):
-        # Everything uses MSB only
-        if self.data[1] is None:
-            self.value = self.data[0]
-        else:
-            self.value = self.data
-
-
-class VoiceChange(StateChange):
-    """
-    A StateChange object for when the voice is changed.
-    """
-
-    def __init__(self, wrapped, bank_program):
-        super().__init__(wrapped)
-        self.bank_program = bank_program
-
-        assert self.message.program == bank_program[2]
-
-        self.value = voices.from_bank_program_default(*bank_program)
-
-    @property
-    def value_longform(self):
-        return self.value.voice_string_extended()
-
-    @property
-    def wrap_type_longform(self):
-        return "Program Change:"
