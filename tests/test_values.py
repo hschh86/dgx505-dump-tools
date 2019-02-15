085b7d7d42a50cf5e372933ee59ae4e9757920a7
xjh <hschh86@users.noreply.github.com>
2019-02-16 02:35:26 +1100

    Tests

--- tests/test_values.py
+++ tests/test_values.py
@@ -1,7 +1,7 @@
 import pytest
 
 from commons import values, maps
-from commons.messages import controls, controlstate, wrappers, voices, chords
+from commons.messages import controls, controlstate, wrappers, voices, chords, styles
 
 
 def test_notes():
@@ -27,11 +27,13 @@ def test_notes():
         values.RootNote(values.NoteAcc.FLAT)
 
     for i, note in values.ROOT_NOTE_SEQ.items():
-        assert values.ENHARMONIA[note] == i
+        assert values.enharmonia(note) == i
     
-    for a, b in [('Cb', 'B♮'), ('F', 'E#'), ('G♯', 'A♭')]:
-        assert (values.ENHARMONIA[values.RootNote.from_name(a)] 
-            == values.ENHARMONIA[values.RootNote.from_name(b)])
+    for p in [('Cb', 'B♮'), ('F', 'E#'), ('G♯', 'A♭')]:
+        assert values.enharmonic(*map(values.RootNote.from_name, p))
+    
+    for p in [('A#', 'B'), ('C', 'B')]:
+        assert not values.enharmonic(*map(values.RootNote.from_name, p))
 
 def test_voices():
     for n in [1, 2, 3, 122, 136, 200, 494]:
@@ -44,3 +46,14 @@ def test_voices():
 def test_chords():
     for n in [0x00, 0x22, 0x2A]:
         assert chords.CHORDS.codes[n].code == n
+
+def test_styles():
+    for n in [1, 2, 8, 22, 135]:
+        style = styles.from_number(n)
+        assert style.number == n
+        assert styles.from_name(style.name) == style
+
+    for n in [0, 136]:
+        with pytest.raises(KeyError):
+            styles.from_number(n)
+            
